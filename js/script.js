c1910c6e2805db010bc051b0ee39540deed1fde9
k025c0017 <k025c0017@g.neec.ac.jp>
2026-01-20 10:50:13 +0900

    8

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
   gsap.set("#kawasaki", { x: "100vw" });
 
   gsap.to("#matsuya", {
-    x: -110,
+    x: -100,
     duration: 3,
     ease: "sine.out"
   });
