8e7fdd1a54bf2cabe2e1ba5118bc5b6f50928aed
k025c0017 <k025c0017@g.neec.ac.jp>
2026-01-20 11:01:19 +0900

    15

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 window.addEventListener("load", () => {
-  gsap.set("#matsuya", { xPercent: "-100vw" });
-  gsap.set("#kawasaki", { xPercent: "100vw" });
+  gsap.set("#matsuya", { xPercent: -100 });
+  gsap.set("#kawasaki", { xPercent: 100 });
 
   gsap.to("#matsuya", {
-    xPercent: -20,
+    xPercent: -30,
     duration: 3,
     ease: "sine.out"
   });
 
   gsap.to("#kawasaki", {
-    xPercent: 100,
+    xPercent: 30,
     duration: 3,
     ease: "sine.out"
   });
-
-
 });
