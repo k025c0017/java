9ef89ae3c9e044ecfb519ab651c804e90bfbe968
k025c0017 <k025c0017@g.neec.ac.jp>
2026-01-20 11:06:49 +0900

    16

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
 window.addEventListener("load", () => {
-  gsap.set("#matsuya", { xPercent: -100 });
-  gsap.set("#kawasaki", { xPercent: 100 });
+  const duration = 3;
+  const easeType = "power3.out";
 
-  gsap.to("#matsuya", {
-    xPercent: -30,
-    duration: 3,
-    ease: "sine.out"
+ 
+  gsap.set("#matsuya", { x: "-100vw", opacity: 0 });
+  gsap.set("#kawasaki", { x: "100vw", opacity: 0 });
+  gsap.set(".title", { opacity: 1 }); 
+  const tl = gsap.timeline();
+
+ 
+  tl.to("#matsuya", {
+    x: 0,
+    opacity: 1,
+    duration: duration,
+    ease: easeType
+  }, 0);
+
+  tl.to("#kawasaki", {
+    x: 0,
+    opacity: 1,
+    duration: duration,
+    ease: easeType
+  }, 0);
+
+  
+  tl.to(".title", {
+    scale: 1.1,
+    duration: 0.4,
+    ease: "power1.out"
   });
 
-  gsap.to("#kawasaki", {
-    xPercent: 30,
-    duration: 3,
-    ease: "sine.out"
+  tl.to(".title", {
+    scale: 1,
+    duration: 0.3,
+    ease: "power1.in"
   });
 });
